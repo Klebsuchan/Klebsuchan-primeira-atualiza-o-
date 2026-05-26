94ab0df6e45aec6497cfa22268a0217369bbb7b5
Braian Kleber <braian.kleber.camargo@gmail.com>
2026-05-26 17:08:10 -0300

    feat: update sitemap schema and fallback date
    Add XML schema definitions to sitemap and include a date fallback for posts to ensure valid sitemap generation.

--- generate_sitemap.cjs
+++ generate_sitemap.cjs
@@ -6,7 +6,9 @@ try {
   const posts = JSON.parse(fs.readFileSync(postsPath, 'utf8'));
 
   const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   <url>
     <loc>https://klebsuchan.com.br/</loc>
     <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
@@ -16,7 +18,7 @@ try {
   ${posts.map(post => `
   <url>
     <loc>https://klebsuchan.com.br/?post=${post.id}</loc>
-    <lastmod>${new Date(post.date).toISOString().split('T')[0]}</lastmod>
+    <lastmod>${new Date(post.date || Date.now()).toISOString().split('T')[0]}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>`).join('')}
